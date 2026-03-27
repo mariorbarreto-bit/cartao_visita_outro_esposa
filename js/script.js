6340461a7b2195fe6c554972de20ea1288ed8af5
mariorbarreto-bit <mariorbarreto@gmail.com>
2026-03-27 15:28:09 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+function downloadImage() {
+    const link = document.createElement('a');
+    link.href = 'cartao_Lucia_Zani_terapeuta.png';
+    link.download = 'cartao_Lucia_Zani_terapeuta.png';
+    link.click();
+}
